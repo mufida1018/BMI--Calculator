a6657978bd79df2eaa5d41163aa572c0c5ef930f
mufida1018 <mufidapatel03@gmail.com>
2023-07-23 23:58:24 +0530

    start project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() => runApp(BmiCalculation());
+
+class BmiCalculation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
